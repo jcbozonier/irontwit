35be5f64ad9dfad64b218fc6746cdbae13c0c111
Justin Bozonier <darkxanthos@gmail.com>
2009-03-15 22:55:25 -0700

    Adding threading helper

---
+++ IronTwit/IronTwit/UI/Utilities/UIThreadManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Threading;
+
+namespace Unite.UI.Utilities
+{
+    public class UIThreadManager
+    {
+        private Thread _Thread;
+
+        public UIThreadManager(Thread thread)
+        {
+            _Thread = thread;
+        }
+
+        public void Execute(Action action)
+        {
+            var dispatcher = Dispatcher.FromThread(_Thread);
+            dispatcher.Invoke(DispatcherPriority.Normal, action);
+        }
+    }
+}

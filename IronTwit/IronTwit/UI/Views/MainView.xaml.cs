8cee1dc59786cf90fb544e9f46ae70da311545c7
TimErickson <tim@in2words.org>
2009-03-15 17:30:53 -0700

    Major UI improvements: Added Minimize, Close control buttons; Added Resize gripper and handling; Fixed bug where window jumped when clicking on message textbox added with window draggability; set credential dialog to center over main window; added [Enter] handling in message TextBox to send message; set focus to Recipient TextBox on main window activation; General layout improvements overall.

--- IronTwit/IronTwit/UI/Views/MainView.xaml.cs
+++ IronTwit/IronTwit/UI/Views/MainView.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Windows;
+using System.Windows.Input;
 using Unite.UI.Utilities;
 using Unite.UI.ViewModels;
 
@@ -20,6 +22,59 @@ namespace Unite.UI.Views
         void Window1_Loaded(object sender, RoutedEventArgs e)
         {
             ((ViewModels.IInitializeView)DataContext).Init();
+        }
+
+        private void Resizer_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Resizer.CaptureMouse();
+            _resizeMouseDownPoint = e.MouseDevice.GetPosition(null);
+            _mouseDownSize = new Size(Width, Height);
+            _resizing = true;
+        }
+
+        private void Resizer_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            _resizing = false;
+            Resizer.ReleaseMouseCapture();
+        }
+
+        private void Resizer_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (!_resizing)
+                return;
+
+            var currentPosition = e.MouseDevice.GetPosition(null);
+            var moveVector = Point.Subtract(currentPosition, _resizeMouseDownPoint);
+
+            if (_resizing)
+            {
+                ResizeWindow(e, moveVector);
+                return;
+            }
+        }
+
+        private void ResizeWindow(MouseEventArgs e, Vector dragVector)
+        {
+            var newWidth = _mouseDownSize.Width  + dragVector.X;
+            var newHeight = _mouseDownSize.Height + dragVector.Y;
+            Width = Math.Max(newWidth, 200);
+            Height = Math.Max(newHeight, 300);
+        }
+
+        private void MinimizeButton_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void CloseButton_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            Close();
+        }
+
+        private void MessageToSend_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+                SendMessage.Command.Execute(null);
         }
     }
 }

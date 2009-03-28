d65ee75edbbe86f733913f16fe6fada77422da03
Justin Bozonier <darkxanthos@gmail.com>
2009-03-28 11:36:56 -0700

    Adding ability to send IMs to google talk users... cant receive yet tho.

---
+++ IronTwit/IronTwit/Plug Ins/GoogleTalkPlugIn/GoogleTalkMessagingService.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using jabber.client;
+using Unite.Messaging;
+using Unite.Messaging.Entities;
+using Unite.Messaging.Messages;
+using Unite.Messaging.Services;
+
+namespace GoogleTalkPlugIn
+{
+    public class GoogleTalkMessagingService : IMessagingService
+    {
+        private JabberClient _Client;
+        private Credentials _Credentials;
+
+        private static readonly ServiceInformation _ServiceInformation = new ServiceInformation()
+                                                             {
+                                                                 ServiceID = Guid.NewGuid(),
+                                                                 ServiceName = "GoogleTalk"
+                                                             };
+
+        public bool CanAccept(Credentials credentials)
+        {
+            return !credentials.UserName.Contains("@");
+        }
+
+        public List<IMessage> GetMessages()
+        {
+            return new List<IMessage>();
+        }
+
+        public void SendMessage(IIdentity recipient, string message)
+        {
+            if(_Credentials == null)
+                if(CredentialsRequested != null)
+                    CredentialsRequested(this, new CredentialEventArgs() { ServiceInfo = _ServiceInformation });
+            if(_Credentials == null)
+                throw new Exception("Your credentials can not still be null. This should NEVER happen.");
+
+            var client = _Client;
+
+            client.AutoPresence = false;
+            client.AutoRoster = false;
+            client.AutoReconnect = -1;
+
+            client.User = _Credentials.UserName;
+            client.Password = _Credentials.Password;
+            client.Server = "gmail.com";
+            client.Port = 5222;
+            client.Resource = "Unit3";
+
+            client.OnAuthError += (s, e) =>
+                                      {
+                                          if(AuthorizationFailed != null)
+                                              AuthorizationFailed(this, new CredentialEventArgs(){ServiceInfo = _ServiceInformation});
+                                      };
+
+            client.OnAuthenticate += s =>
+                                         {
+                                             client.Message(recipient.UserName, message);
+                                         };
+            client.Connect();
+        }
+
+        public void SetCredentials(Credentials credentials)
+        {
+            _Credentials = credentials;
+        }
+
+        public event EventHandler<CredentialEventArgs> CredentialsRequested;
+        public event EventHandler<CredentialEventArgs> AuthorizationFailed;
+
+        public bool CanFind(string address)
+        {
+            // If the address contains an ampersand it can't start with it
+            // or it just shouldn't have one at all.
+            return (!address.StartsWith("@") && address.Contains("@")) || !address.Contains("@");
+        }
+
+        public ServiceInformation GetInformation()
+        {
+            return _ServiceInformation;
+        }
+
+        public void StartReceiving()
+        {
+            //throw new System.NotImplementedException();
+        }
+
+        public void StopReceiving()
+        {
+            //throw new System.NotImplementedException();
+        }
+
+        public event EventHandler<MessagesReceivedEventArgs> MessagesReceived;
+    }
+}
---
+++ IronTwit/IronTwit/Unite.TestHarness/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using jabber.client;
+
+namespace Unite.TestHarness
+{
+    class Program
+    {
+        private static ManualResetEvent done = new ManualResetEvent(false);
+        static void Main(string[] args)
+        {
+            var client = new JabberClient();
+
+            client.AutoPresence = false;
+            client.AutoRoster = false;
+            client.AutoReconnect = -1;
+
+            client.User = "herbstudent";
+            client.Password = "samhain8";
+            client.Server = "gmail.com";
+            client.Port = 5222;
+            client.Resource = "Pidgin";
+
+            client.OnMessage += (s, e) =>
+                                    {
+                                        Console.WriteLine("Message sent...");
+                                    };
+
+            client.OnAuthError += (s, e) =>
+                                      {
+                                          Console.WriteLine("Auth Error: " + e.InnerText);
+                                          client.Close();
+                                      };
+
+            client.OnAuthenticate += s =>
+                                         {
+                                             Console.WriteLine("Authenticated...");
+                                             client.Message("darkxanthos@gmail.com", "Testing");
+                                         };
+
+            client.OnError += (s, e) =>
+                                  {
+                                      Console.WriteLine("Error! " + e.Message);
+                                      client.Close();
+                                  };
+
+            
+
+            client.Connect();
+
+            done.WaitOne();
+            client.Close();
+        }
+
+        static void client_OnError(object sender, Exception ex)
+        {
+            
+        }
+
+        static void client_OnMessage(object sender, jabber.protocol.client.Message msg)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

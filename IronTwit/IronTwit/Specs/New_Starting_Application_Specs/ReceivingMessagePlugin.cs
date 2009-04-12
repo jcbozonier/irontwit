3f79aae95894907e4c26a5b83f971368691e69f9
Justin Bozonier <darkxanthos@gmail.com>
2009-04-11 20:42:54 -0700

    Checking in to move to new home of Alloy.

--- IronTwit/IronTwit/Specs/New_Starting_Application_Specs/ReceivingMessagePlugin.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using Unite.Messaging.Entities;
-using Unite.Messaging.Messages;
-using Unite.Messaging.Services;
-
-namespace Unite.Specs.New_Starting_Application_Specs
-{
-
-}
---
+++ IronTwit/IronTwit/Specs/New_Starting_Application_Specs/Sending_message.cs
@@ -0,0 +1,10 @@
+﻿using NUnit.Framework;
+
+namespace Unite.Specs.New_Starting_Application_Specs
+{
+    [TestFixture]
+    public class When_sending_a_message
+    {
+       
+    }
+}
